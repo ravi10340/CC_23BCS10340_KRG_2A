dd59066202f49add7ae49b7d7b173f8d81d3d737
ravi10340 <rvin3355@gmail.com>
2026-04-02 22:02:31 +0530

    Create DSU.java

---
+++ SESSIONWISE_PROBLEMS/DSU.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class DSU {
+    int[] parent, rank;
+
+    DSU(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+
+        if (px == py) return;
+
+        if (rank[px] < rank[py]) {
+            parent[px] = py;
+        } else if (rank[px] > rank[py]) {
+            parent[py] = px;
+        } else {
+            parent[py] = px;
+            rank[px]++;
+        }
+    }
+}
