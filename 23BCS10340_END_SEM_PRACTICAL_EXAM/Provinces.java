c9478d1fa543996571fbd17df380db3a36ecee62
ravi10340 <rvin3355@gmail.com>
2026-04-25 16:04:41 +0530

    Create Provinces.java

---
+++ 23BCS10340_END_SEM_PRACTICAL_EXAM/Provinces.java
@@ -0,0 +1,36 @@
+public class nprovinces {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                provinces++;
+                dfs(isConnected, visited, i);
+            }
+        }
+
+        return provinces;
+    }
+
+    private void dfs(int[][] isConnected, boolean[] visited, int node) {
+        visited[node] = true;
+        for (int neighbor = 0; neighbor < isConnected.length; neighbor++) {
+            if (isConnected[node][neighbor] == 1 && !visited[neighbor]) {
+                dfs(isConnected, visited, neighbor);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        nprovinces sol = new nprovinces();
+        int[][] isConnected = {
+            {1, 1, 0},
+            {1, 1, 0},
+            {0, 0, 1}
+        };
+        System.out.println("No Of Provinces : "+ sol.findCircleNum(isConnected));
+    }
+}
+
